1c4bec39356b2097ae515314d1e33c7df94ad970
Sophist <github@sodalis.co.uk>
2018-04-23 21:02:35 +0100

    Make namespace work with requirejs

--- administrator/components/com_fabrik/views/namespace.js
+++ administrator/components/com_fabrik/views/namespace.js
@@ -5,9 +5,9 @@
  * @license:   GNU/GPL http://www.gnu.org/copyleft/gpl.html
  */
 
-(function () {
+define(['jquery'], function ($) {
 	if (typeOf(window.FabrikAdmin) === 'object') {
-		return;
+		return FabrikAdmin;
 	}
 	FabrikAdmin = {};
 
@@ -25,7 +25,7 @@
 			}
 		});
 		var JTooltips = new Tips($$('.hasTip'), { maxTitleChars: 50, fixed: false});
-		
+
 		// Joomla3.2
 		if (typeof(jQuery) !== 'undefined') {
 			jQuery('.hasTooltip').tooltip({'html': true, 'container': 'body'});
@@ -34,27 +34,23 @@
 	};
 
 	window.fireEvent('fabrik.admin.namespace');
-}());
-
-if (typeof(jQuery) !== 'undefined') {
 
 	// Relay radio button group clicks for content added via ajax calls
-	(function ($) {
-		$(document).on('click', '.btn-group label:not(.active)', null, function (event) {
-			var label = $(this);
-			var input = $('#' + label.attr('for'));
-			if (!input.prop('checked')) {
-				label.closest('.btn-group').find("label").removeClass('active btn-success btn-danger btn-primary');
-				if (input.val() === '') {
-					label.addClass('active btn-primary');
-				} else if (input.val().toInt() === 0) {
-					label.addClass('active btn-danger');
-				} else {
-					label.addClass('active btn-success');
-				}
-				input.prop('checked', true);
-                input.trigger('change');
+	$(document).on('click', '.btn-group label:not(.active)', null, function (event) {
+		var label = $(this);
+		var input = $('#' + label.attr('for'));
+		if (!input.prop('checked')) {
+			label.closest('.btn-group').find("label").removeClass('active btn-success btn-danger btn-primary');
+			if (input.val() === '') {
+				label.addClass('active btn-primary');
+			} else if (input.val().toInt() === 0) {
+				label.addClass('active btn-danger');
+			} else {
+				label.addClass('active btn-success');
 			}
-		});
-	})(jQuery);
-}
+			input.prop('checked', true);
+			input.trigger('change');
+		}
+	});
+	return FabrikAdmin;
+});
